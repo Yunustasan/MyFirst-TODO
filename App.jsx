549fa279e59a499a8acd75ab90c84080fb8e49a8
Yunus Emre Tasan <141951352+Yunustasan@users.noreply.github.com>
2023-12-30 22:33:19 +0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+
+  const [newItem, setNewItem] = useState("");
+  const [items, setItems] = useState([]);
+
+
+  function addItem() {
+
+    if (!newItem) {
+      alert("enter a note");
+      return;
+    }
+
+    const item = {
+      id: Math.floor(Math.random() * 1000),
+      value: newItem
+    };
+
+    setItems((oldList) => [...oldList, item]);
+
+    setNewItem("");
+    console.log(items)
+
+  }
+
+  function deleteItem(id) {
+    const newArray = items.filter(item => item.id !== id);
+    setItems(newArray);
+  }
+
+  return (
+    <div className="App">
+      <h1 className='Head' >Todo List App</h1>
+      <form onSubmit={(e) => {
+        e.preventDefault();
+        addItem();
+      }}>
+        <input
+          type="text"
+          placeholder='Add your notes'
+          value={newItem}
+          onChange={e => setNewItem(e.target.value)}
+        />
+      </form>
+      <button onClick={(e) => addItem()}>Add</button>
+
+      <ul>
+        {items.map(item => {
+          return <li key={item.id}> {item.value} <button onClick={e => deleteItem(item.id)} className='btn' >Delete</button>   </li>
+        })}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
